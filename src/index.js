bc0b3d8738ef95132fd32789fa40dbdbb96e738a
rohitGupta2022 <pta.rohit28@gmail.com>
2023-03-10 11:16:28 +0530

    added sidebar

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import { ColorModeScript } from '@chakra-ui/react';
+import { ColorModeScript, theme, ChakraProvider } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
+import ColorModeSwitcher from './ColorModeSwitcher';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
@@ -9,6 +10,9 @@ const root = ReactDOM.createRoot(container);
 root.render(
   <StrictMode>
     <ColorModeScript />
-    <App />
+    <ChakraProvider theme={theme}>
+      <ColorModeSwitcher />
+      <App />
+    </ChakraProvider>
   </StrictMode>
 );
